b0a0e2a744abf63ee0dbf446a501fa7adf828b7b
dquang0504 <deadlygamer686@gmail.com>
2025-07-28 21:59:52 +0700

    Created the VNPay gateway url to redirect user when clicked Place Order

---
+++ GoodFood-BE/config/vnpay_config.go
@@ -0,0 +1,81 @@
+package config
+
+import (
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
+	"fmt"
+	"math/rand"
+	"sort"
+	"strings"
+	"time"
+)
+
+const (
+	VnpVersion   = "2.1.0"
+	VnpCommand   = "pay"
+	VnpPayURL    = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html"
+	VnpReturnURL = "https://fivefood.shop/vnpay_return"
+	VnpTmnCode   = "GHFBIZ5U"
+	SecretKey    = "ZFSNKTR0QRTMJWOSXVYQNM6QYI0KPV05"
+	VnpApiURL    = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction"
+	Vnp_IpAddr   = "127.0.0.1"
+	VnpCurrCode  = "VND"
+	VnpBankCode  = "NCB"
+	VnpLocale    = "vn"
+	
+)
+
+//MD5 Hash
+func MD5(message string) string{
+	hash := md5.Sum([]byte(message))
+	return hex.EncodeToString(hash[:])
+}
+
+// SHA256 hash
+func Sha256(message string) string{
+	hash := sha256.Sum256([]byte(message))
+	return hex.EncodeToString(hash[:])
+}
+
+// HMAC SHA512
+func HmacSHA512(key, data string) string {
+	h := hmac.New(sha512.New, []byte(key))
+	h.Write([]byte(data))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+// HashAllFields: tạo chuỗi query string rồi HMAC SHA512
+func HashAllFields(fields map[string]string) string {
+	var keys []string
+	for k := range fields {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	var sb strings.Builder
+	for i, k := range keys {
+		v := fields[k]
+		if v != "" {
+			sb.WriteString(fmt.Sprintf("%s=%s", k, v))
+			if i < len(keys)-1 {
+				sb.WriteString("&")
+			}
+		}
+	}
+
+	return HmacSHA512(SecretKey, sb.String())
+}
+
+// GetRandomNumber: tạo chuỗi số ngẫu nhiên
+func GetRandomNumber(length int) string {
+	const digits = "0123456789"
+	rand.Seed(time.Now().UnixNano())
+	sb := make([]byte, length)
+	for i := range sb {
+		sb[i] = digits[rand.Intn(len(digits))]
+	}
+	return string(sb)
+}
--- GoodFood-BE/internal/server/handlers/invoices.go
+++ GoodFood-BE/internal/server/handlers/invoices.go
@@ -1,8 +1,14 @@
 package handlers
 
 import (
+	"GoodFood-BE/config"
 	"GoodFood-BE/internal/service"
 	"GoodFood-BE/models"
+	"fmt"
+	"net/url"
+	"sort"
+	"strconv"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/volatiletech/sqlboiler/v4/boil"
@@ -44,4 +50,78 @@ func InvoicePay(c *fiber.Ctx) error{
 
 	return c.JSON(resp);
 
+}
+
+func InvoiceOnlinePay(c *fiber.Ctx) error{
+
+	body := models.Invoice{}
+	if err := c.BodyParser(&body); err != nil{
+		return service.SendError(c,400,err.Error());
+	}
+
+	//amount * 100 (vnpay requirement)
+	amount := int64(body.TotalPrice) * 100
+	orderId := strconv.Itoa(body.InvoiceID + 1)
+
+	//query params
+	vnpParams := map[string]string{
+		"vnp_Version": 	 config.VnpVersion,
+		"vnp_Command":   config.VnpCommand,
+		"vnp_TmnCode":   config.VnpTmnCode,
+		"vnp_Amount":    strconv.FormatInt(amount, 10),
+		"vnp_CurrCode":  "VND",
+		"vnp_BankCode":  "NCB",
+		"vnp_TxnRef":    orderId,
+		"vnp_OrderInfo": fmt.Sprintf("Paying for invoice: %d", body.InvoiceID),
+		"vnp_Locale":    "vn",
+		"vnp_OrderType": "other",
+		"vnp_ReturnUrl": "https://fivefood.shop/vnpay_return",
+		"vnp_IpAddr":    "127.0.0.1",
+	}
+
+	//Time zone in Asia/HCM
+	loc, _ := time.LoadLocation("Asia/Ho_Chi_Minh")
+	now := time.Now().In(loc)
+	vnpParams["vnp_CreateDate"] = now.Format("20060102150405")
+	vnpParams["vnp_ExpireDate"] = now.Add(15 * time.Minute).Format("20060102150405")
+
+	// Sort keys before encoding
+	var keys []string
+	for k := range vnpParams {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	//Build query string
+	var query url.Values = url.Values{}
+	var rawData string
+	for i,k := range keys{
+		v :=vnpParams[k]
+		if v != ""{
+			query.Add(k,v)
+			rawData += k + "=" + v
+			if i < len(keys) - 1{
+				rawData += "&"
+			}
+		}
+	}
+
+	// Hash HMAC SHA512
+	secureHash := config.HmacSHA512(config.SecretKey, rawData)
+	query.Add("vnp_SecureHash", secureHash)
+
+	// Build final URL
+	paymentUrl := fmt.Sprintf("%s?%s", config.VnpPayURL, query.Encode())
+
+	fmt.Println("VNPay RawData:", rawData)
+	fmt.Println("VNPay SecureHash:", secureHash)
+
+
+	resp := fiber.Map{
+		"status": "Success",
+		"data": paymentUrl,
+		"message": "Successfully redirected to VNPay gateway!",
+	}
+
+	return c.JSON(resp);
 }
